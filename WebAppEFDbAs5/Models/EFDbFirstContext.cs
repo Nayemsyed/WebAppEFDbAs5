e3206567a334ed08361c28f631e22a8e3d0f719c
Nayemsyed <sdnayem04112000@gmail.com>
2024-02-06 15:50:00 +0530

    Add project files.

---
+++ WebAppEFDbAs5/Models/EFDbFirstContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAppEFDbAs5.Models
+{
+    public partial class EFDbFirstContext : DbContext
+    {
+        public EFDbFirstContext()
+        {
+        }
+
+        public EFDbFirstContext(DbContextOptions<EFDbFirstContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Player> Players { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=DESKTOP-6COE67H;database=EFDbFirst;trusted_connection=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Player>(entity =>
+            {
+                entity.Property(e => e.PlayerId).ValueGeneratedNever();
+
+                entity.Property(e => e.Fname)
+                    .HasMaxLength(100)
+                    .HasColumnName("FName");
+
+                entity.Property(e => e.Lname)
+                    .HasMaxLength(100)
+                    .HasColumnName("LName");
+
+                entity.Property(e => e.Team).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ WebAppEFDbAs5/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAppEFDbAs5.Models
+{
+    public partial class Player
+    {
+        public int PlayerId { get; set; }
+        public string Fname { get; set; } = null!;
+        public string Lname { get; set; } = null!;
+        public int? JerseyNumber { get; set; }
+        public int? Position { get; set; }
+        public string Team { get; set; } = null!;
+    }
+}
